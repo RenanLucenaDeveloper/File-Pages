77a98eda38a0771e5ca0ae8aaba4a153c301fcf9
Renan Lucena <reenandeveloper@hotmail.com>
2022-07-17 11:43:14 -0300

    beta v8

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-import initIndex from "./modules/index.js";
 import initPagesCreator from "./modules/pages-creator.js";
 import initPresentation from "./modules/presentation.js";
 
-function index(){
-  initIndex();
+function initIndex() {
 
+  const styleButton = document.querySelector("#dropdown-button");
   const initButton = document.querySelector('[data-slide="init"]');
+  const acceptCookies = document.querySelector('[data-cookies="accept"]');
+  const styleOptions = document.querySelectorAll("#dropdown-content li");
+  
+  styleButton.addEventListener("click", () => {
+    styleButton.parentElement.classList.toggle('ativo');
+  });
 
   initButton.addEventListener("click", () => {
     const div = document.querySelectorAll("div");
@@ -16,12 +21,43 @@ function index(){
     h1.forEach((item) => {
       item.remove();
     });
-  
-    function pagesCreatorAndPresentation(){
+    
+    function initPagesCreatorAndPresentation(){
       initPagesCreator();
       initPresentation();
-    } pagesCreatorAndPresentation();
-
+    } initPagesCreatorAndPresentation();
   });
 
-} index();
+  function initPrepareOptions() {
+    styleOptions[0].classList.add("ativo");
+
+    const correct = document.createElement("img");
+    correct.setAttribute("src", "./img/ativo.svg");
+
+    styleOptions[0].appendChild(correct);
+
+    styleOptions[0].__proto__.changePageStyle = function () {
+      console.log(styleOptions[0].__proto__);
+    }; styleOptions[0].__proto__.changePageStyle();
+
+    styleOptions.forEach((e) => {
+      e.addEventListener("click", () => {
+        let activeElement = document.querySelector("#dropdown-content .ativo");
+
+        activeElement.classList.remove("ativo");
+
+        activeElement.removeChild(correct);
+
+        e.classList.add("ativo");
+        e.appendChild(correct);
+      });
+    });
+  }
+  initPrepareOptions();
+
+  acceptCookies.addEventListener("click", () => {
+    acceptCookies.innerText = "ACEITO";
+    console.log("cookies accepted");
+  });
+}
+initIndex();
